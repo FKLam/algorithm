77268b705bc4bc1ca24e350b3f9c27f7782fddc2
linfk <linfk@xiaopeng.com>
2022-03-11 18:36:19 +0800

    6. Z 字形变换

---
+++ towSum/CovertSolution.cpp
@@ -0,0 +1,24 @@
+//
+//  CovertSolution.cpp
+//  towSum
+//
+//  Created by Xpeng on 2022/3/11.
+//
+
+#include "CovertSolution.hpp"
+
+/*
+ 6. Z 字形变换
+ 将一个给定字符串 s 根据给定的行数 numRows ，以从上往下、从左到右进行 Z 字形排列。
+ 比如输入字符串为 "PAYPALISHIRING" 行数为 3 时，排列如下：
+ P   A   H   N
+ A P L S I I G
+ Y   I   R
+ 之后，你的输出需要从左往右逐行读取，产生出一个新的字符串，比如："PAHNAPLSIIGYIR"。
+ */
+
+string CovertSolution::convert(string s, int numRows) {
+    long size = s.size();                       // 字符串长度
+    
+    return "";
+}
---
+++ towSum/CovertSolution.hpp
@@ -0,0 +1,21 @@
+//
+//  CovertSolution.hpp
+//  towSum
+//
+//  Created by Xpeng on 2022/3/11.
+//
+
+#ifndef CovertSolution_hpp
+#define CovertSolution_hpp
+
+#include <stdio.h>
+#include <string>
+
+using namespace std;
+
+class CovertSolution {
+public:
+    string convert(string s, int numRows);
+};
+
+#endif /* CovertSolution_hpp */
